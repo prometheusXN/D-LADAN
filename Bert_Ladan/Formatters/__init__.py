00c572b2cbccba5a131974298fa81cc00d4644a8
Nuo Xu <prometheusXN@users.noreply.github.com>
2024-08-31 20:35:16 +0800

    Create __init__.py

---
+++ Bert_Ladan/Formatters/__init__.py
@@ -0,0 +1,2 @@
+from .SentenceLevelFormatter import SentenceFormatter, SentenceFormatter_C, SentenceFormatter_D
+from .SentenceLevelFormatter import collate_fn_fact, SentenceFormatter_DC, SentenceFormatter_W2V
